382a6729f634902b2610db7050be419c8d5e6363
Aleksandr Khristichenko <alexanderhristichenko@yandex.ru>
2022-05-08 13:49:09 +0300

    feat: Add tab bar

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let appColor: UIColor = .systemTeal
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyViewController()
+    let mainViewController = MainViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
@@ -26,10 +29,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingContainerViewController.delegate = self
         dummyViewController.logoutDelegate = self
         
-        window?.rootViewController = loginViewController
+        window?.rootViewController = mainViewController
+        //window?.rootViewController = loginViewController
         //window?.rootViewController = onboardingContainerViewController
         //window?.rootViewController = OnboardingViewController(heroImageName: "", titleText: "23yhh")
         
+        mainViewController.selectedIndex = 1
         return true
     }
     
@@ -49,8 +54,6 @@ extension AppDelegate {
         window.makeKeyAndVisible()
         UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve, animations: nil, completion: nil)
     }
-    
-    
 }
 
 extension AppDelegate: LoginViewControllerDelegate {
