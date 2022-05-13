ca33b57a4f5d920a4be8933223c327382695a58e
Aleksandr Khristichenko <alexanderhristichenko@yandex.ru>
2022-05-13 17:35:55 +0300

    feat: create new password project

---
+++ Password/Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Александр Христиченко on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
